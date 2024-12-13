ed22b44de9ef2a8036fc0815c236fbcaeb2e1ddb
smpimentel <61305859+smpimentel@users.noreply.github.com>
2024-12-13 12:27:28 -0600

    Update vite.config.ts
    added base location

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/lookup-table-generator-dynamic/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
